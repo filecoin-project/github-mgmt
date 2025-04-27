76d50980ea26b8e809c32b78f53f14a0b7592b1b
galargh <piotr.galar@gmail.com>
2025-04-27 12:31:40 +0200

    Revert "feat: add web3-bot to all rust repositories"
    This reverts commit e32a0bfc636611bd087cf52f8d9edba15b2dd5ed.

--- scripts/src/actions/fix-yaml-config.ts
+++ scripts/src/actions/fix-yaml-config.ts
@@ -1,35 +1,20 @@
 import 'reflect-metadata'
 import {Repository} from '../resources/repository.js'
-import {Permission} from '../resources/repository-collaborator.js'
 import {runFormat} from './shared/format.js'
-import {runAddCollaboratorToAllRepos} from './shared/add-collaborator-to-all-repos.js'
 import {runSetPropertyInAllRepos} from './shared/set-property-in-all-repos.js'
 import {runToggleArchivedRepos} from './shared/toggle-archived-repos.js'
 import {runDescribeAccessChanges} from './shared/describe-access-changes.js'
 
 import * as core from '@actions/core'
-import {GitHub} from '../github.js'
 
 function isPublic(repository: Repository) {
   return repository.visibility === 'public'
 }
 
 async function run() {
-  const github = await GitHub.getGitHub()
-  const repositories = await github.listRepositories()
-  const isRust = (repository: Repository) => {
-    return (
-      repositories.find(r => r.name === repository.name)?.language === 'Rust'
-    )
-  }
-
   await runSetPropertyInAllRepos('secret_scanning', true, r => isPublic(r))
   await runSetPropertyInAllRepos('secret_scanning_push_protection', true, r =>
     isPublic(r)
-  )
-  // Ensure all Rust repos have a consistent superuser per https://github.com/filecoin-project/github-mgmt/issues/104
-  await runAddCollaboratorToAllRepos('filecoin-helper', Permission.Push, r =>
-    isRust(r)
   )
   await runToggleArchivedRepos()
   const accessChangesDescription = await runDescribeAccessChanges()
