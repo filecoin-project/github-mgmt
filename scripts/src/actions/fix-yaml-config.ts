9426a7ab51530ce6ddb0ec46ca97c7614470014f
galargh <piotr.galar@gmail.com>
2025-04-28 13:00:08 +0200

    Revert "feat: automatically remove filecoin-helper from all teams and repos"
    This reverts commit 09fdd63fd20e356f6c4e31468696b1ee767f7e3e.

--- scripts/src/actions/fix-yaml-config.ts
+++ scripts/src/actions/fix-yaml-config.ts
@@ -1,48 +1,27 @@
 import 'reflect-metadata'
 import {Repository} from '../resources/repository.js'
-import {RepositoryCollaborator} from '../resources/repository-collaborator.js'
 import {runFormat} from './shared/format.js'
 import {runSetPropertyInAllRepos} from './shared/set-property-in-all-repos.js'
 import {runToggleArchivedRepos} from './shared/toggle-archived-repos.js'
 import {runDescribeAccessChanges} from './shared/describe-access-changes.js'
 
 import * as core from '@actions/core'
-import {Config} from '../yaml/config.js'
-import {TeamMember} from '../resources/team-member.js'
 
 function isPublic(repository: Repository) {
   return repository.visibility === 'public'
 }
 
-async function removeFilecoinHelperFromAllTeamsAndRepos() {
-  const config = Config.FromPath()
-
-  const collaborators = config
-    .getResources(RepositoryCollaborator)
-    .filter(c => c.username === 'filecoin-helper')
-
-  for (const collaborator of collaborators) {
-    config.removeResource(collaborator)
-  }
-
-  const teamMembers = config
-    .getResources(TeamMember)
-    .filter(tm => tm.username === 'filecoin-helper')
-
-  for (const teamMember of teamMembers) {
-    config.removeResource(teamMember)
-  }
-
-  config.save()
-}
-
 async function run() {
-  await runSetPropertyInAllRepos('secret_scanning', true, r => isPublic(r))
-  await runSetPropertyInAllRepos('secret_scanning_push_protection', true, r =>
-    isPublic(r)
+  await runSetPropertyInAllRepos(
+    'secret_scanning',
+    true,
+    r => isPublic(r)
   )
-  // Ensure filecoin-helper is not a collaborator or a team member anywhere, as per https://github.com/filecoin-project/github-mgmt/issues/104
-  await removeFilecoinHelperFromAllTeamsAndRepos()
+  await runSetPropertyInAllRepos(
+    'secret_scanning_push_protection',
+    true,
+    r => isPublic(r)
+  )
   await runToggleArchivedRepos()
   const accessChangesDescription = await runDescribeAccessChanges()
   core.setOutput(
